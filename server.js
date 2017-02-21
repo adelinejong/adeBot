596958bcc00da5feb87f49f15aae31b768c12b7a
adelinejong <adelinejong.2015@smu.edu.sg>
2017-02-21 11:50:29 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,93 @@
+// Reference the packages we require so that we can use them in creating the bot
+var restify = require('restify');
+var builder = require('botbuilder');
+var rp = require('request-promise');
+var BINGSEARCHKEY = 'b6692d8a9c424bd98148f7a0e39ca283';
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+// Listen for any activity on port 3978 of our local server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+// If a Post request is made to /api/messages on port 3978 of our local server, then we pass it to the bot connector to handle
+server.post('/api/messages', connector.listen());
+
+var luisRecognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/073e43cf-a5a6-4914-a197-297c433a03f3?subscription-key=ed6a31ca35044f84a68f77fb07291f99');
+var intentDialog = new builder.IntentDialog({recognizers: [luisRecognizer]});
+
+intentDialog.matches(/\b(hi|hello|hey|howdy)\b/i, '/sayHi')
+    .matches('getNews', '/topNews')
+    .matches('analyseImage', '/analyseImage')
+    .onDefault(builder.DialogAction.send("Sorry, I didn't understand what you said."));
+
+bot.dialog('/', intentDialog);
+
+bot.dialog('/sayHi', function(session) {
+    session.send('Hi there!  Try saying things like "Get news in Toyko"');
+    session.endDialog();
+});
+
+bot.dialog('/topNews', [
+    function (session){
+        // Ask the user which category they would like
+        // Choices are separated by |
+        builder.Prompts.choice(session, "Which category would you like?", "Technology|Science|Sports|Business|Entertainment|Politics|Health|World|(quit)");
+    }, function (session, results, next){
+        // The user chose a category
+        if (results.response && results.response.entity !== '(quit)') {
+           //Show user that we're processing their request by sending the typing indicator
+            session.sendTyping();
+            // Build the url we'll be calling to get top news
+            var url = "https://api.cognitive.microsoft.com/bing/v5.0/news/?" + "category=" + results.response.entity + "&count=10&mkt=en-US&originalImg=true";
+            // Build options for the request
+            var options = {
+                uri: url,
+                headers: {
+                    'Ocp-Apim-Subscription-Key': BINGSEARCHKEY
+                },
+                json: true // Returns the response in json
+            }
+            //Make the call
+            rp(options).then(function (body){
+                // The request is successful
+                console.log(body); // Prints the body out to the console in json format
+                session.send("Managed to get your news.");
+            }).catch(function (err){
+                // An error occurred and the request failed
+                console.log(err.message);
+                session.send("Argh, something went wrong. :( Try again?");
+            }).finally(function () {
+                // This is executed at the end, regardless of whether the request is successful or not
+                session.endDialog();
+            });
+        } else {
+            // The user choses to quit
+            session.endDialog("Ok. Mission Aborted.");
+        }
+    }
+]);
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+// This is called the root dialog. It is the first point of entry for any message the bot receives
+/*
+bot.dialog('/', function (session) {
+    // Send 'hello world' to the user
+    session.send("Hello World, my name is ade");
+});
+*/
+
