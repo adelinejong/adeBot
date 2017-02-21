e5de722ae0dd1fa5801ba6f351b1bdba607d0e38
adelinejong <adelinejong.2015@smu.edu.sg>
2017-02-21 12:42:27 +0800

    forth

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// Reference the packages we require so that we can use them in creating the bot
 var restify = require('restify');
 var builder = require('botbuilder');
 var rp = require('request-promise');
+
+// Static variables that we can use anywhere in server.js
 var BINGSEARCHKEY = 'b6692d8a9c424bd98148f7a0e39ca283';
 
 //=========================================================
@@ -23,18 +26,23 @@ var bot = new builder.UniversalBot(connector);
 // If a Post request is made to /api/messages on port 3978 of our local server, then we pass it to the bot connector to handle
 server.post('/api/messages', connector.listen());
 
-var luisRecognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/073e43cf-a5a6-4914-a197-297c433a03f3?subscription-key=ed6a31ca35044f84a68f77fb07291f99');
+var luisRecognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/eccdfdf9-5aae-4926-b795-05453360e82f?subscription-key=ed6a31ca35044f84a68f77fb07291f99');
 var intentDialog = new builder.IntentDialog({recognizers: [luisRecognizer]});
 
 intentDialog.matches(/\b(hi|hello|hey|howdy)\b/i, '/sayHi')
-    .matches('getNews', '/topNews')
-    .matches('analyseImage', '/analyseImage')
+    .matches("getNews", '/topNews')
     .onDefault(builder.DialogAction.send("Sorry, I didn't understand what you said."));
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+// This is called the root dialog. It is the first point of entry for any message the bot receives
 
 bot.dialog('/', intentDialog);
 
 bot.dialog('/sayHi', function(session) {
-    session.send('Hi there!  Try saying things like "Get news in Toyko"');
+    session.send('Hi there!  Try saying things like "Give me news in Singapore"');
     session.endDialog();
 });
 
@@ -49,7 +57,8 @@ bot.dialog('/topNews', [
            //Show user that we're processing their request by sending the typing indicator
             session.sendTyping();
             // Build the url we'll be calling to get top news
-            var url = "https://api.cognitive.microsoft.com/bing/v5.0/news/?" + "category=" + results.response.entity + "&count=10&mkt=en-US&originalImg=true";
+            var url = "https://api.cognitive.microsoft.com/bing/v5.0/news/?" 
+                + "category=" + results.response.entity + "&count=10&mkt=en-US&originalImg=true";
             // Build options for the request
             var options = {
                 uri: url,
@@ -62,7 +71,7 @@ bot.dialog('/topNews', [
             rp(options).then(function (body){
                 // The request is successful
                 console.log(body); // Prints the body out to the console in json format
-                session.send("Managed to get your news.");
+                sendTopNews(session, results, body);
             }).catch(function (err){
                 // An error occurred and the request failed
                 console.log(err.message);
@@ -77,3 +86,34 @@ bot.dialog('/topNews', [
         }
     }
 ]);
+// This function processes the results from the API call to category news and sends it as cards
+function sendTopNews(session, results, body){
+    session.send("Top news in " + results.response.entity + ": ");
+    //Show user that we're processing by sending the typing indicator
+    session.sendTyping();
+    // The value property in body contains an array of all the returned articles
+    var allArticles = body.value;
+    var cards = [];
+    // Iterate through all 10 articles returned by the API
+    for (var i = 0; i < 10; i++){
+        var article = allArticles[i];
+        // Create a card for the article and add it to the list of cards we want to send
+        cards.push(new builder.HeroCard(session)
+            .title(article.name)
+            .subtitle(article.datePublished)
+            .images([
+                //handle if thumbnail is empty
+                builder.CardImage.create(session, article.image.contentUrl)
+            ])
+            .buttons([
+                // Pressing this button opens a url to the actual article
+                builder.CardAction.openUrl(session, article.url, "Full article")
+            ]));
+    }
+    var msg = new builder.Message(session)
+        .textFormat(builder.TextFormat.xml)
+        .attachmentLayout(builder.AttachmentLayout.carousel)
+        .attachments(cards);
+    session.send(msg);
+}
+
